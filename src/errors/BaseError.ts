0deabe01e52e595dc672f9d1543257cd82c5262f
marcos-silva <dusdoom.dev@gmail.com>
2022-08-27 17:32:59 -0300

    refactor(errors): changed BaseError property type

--- src/errors/BaseError.ts
+++ src/errors/BaseError.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 /**Abstract class used to create Errors that will be throwed inside de controllers.*/
 export default abstract class BaseError extends Error {
   /**http status code to be sent to the client @see {@link StatusCodes} */
-  public status: number | null = null;
+  public status: number;
 
   constructor(
     /**Custom name identifier */
