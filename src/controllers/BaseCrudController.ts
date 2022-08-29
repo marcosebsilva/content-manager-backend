ef2f1952fb96a37b1225c899471fa99465532fdd
marcos-silva <dusdoom.dev@gmail.com>
2022-08-29 16:25:11 -0300

    refactor(controllers): refactored BaseCrud into a abstract class to allow use of static

--- src/controllers/BaseCrudController.ts
+++ src/controllers/BaseCrudController.ts
@@ -1,19 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction } from 'express';
 
-/**Base interface to define a CRUD controller, no matter the database */
-export default interface BaseCrudController<T> {
-  /**Handles request to create new assets on the database based on the specified model {@link T} */
-  create: (req: Request , res: Response, next: NextFunction) => void
+/** Base interface to define a CRUD controller, no matter the database */
+export default abstract class BaseCrudController {
+  /** Handles request to create new assets on the database based on the specified model  */
+  static create(_req: Request, _res: Response, _next: NextFunction) {
+    throw new Error("Method 'create' must be overwritten");
+  }
 
-  /**Handles request to get one specific asset from the specified model {@link T} */
-  getOne: (req: Request , res: Response, next: NextFunction) => void
+  /** Handles request to get one specific asset from the specified model  */
+  static getOne(req: Request, res: Response, next: NextFunction) {
+    throw new Error("Method 'getOne' must be overwritten");
+  }
 
-  /**Handles request to get all available assets for the specified model {@link T} */
-  getAll: (req: Request , res: Response, next: NextFunction) => void
+  /** Handles request to get all available assets for the specified model  */
+  static getAll(req: Request, res: Response, next: NextFunction) {
+    throw new Error("Method 'getAll' must be overwritten");
+  }
 
-  /**Handles request to update one specific asset in the {@link T} model  */
-  update: (req: Request , res: Response, next: NextFunction) => void
+  /** Handles request to update one specific asset in the  model  */
+  static update(req: Request, res: Response, next: NextFunction) {
+    throw new Error("Method 'update' must be overwritten");
+  }
 
-  /** Handles request to delete one specific asset in the {@link T} model */
-  delete: (req: Request , res: Response, next: NextFunction) => void
+  /** Handles request to delete one specific asset in the  model */
+  static delete(req: Request, res: Response, next: NextFunction) {
+    throw new Error("Method 'delete' must be overwritten");
+  }
 }
--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,88 +1,100 @@
-import PostModel, { IPost } from "../models/PostModel";
-import { Request, Response, NextFunction } from "express";
-import NotFoundError from "../errors/NotFoundError";
-import BaseCrudController from "./BaseCrudController";
+import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { isValidObjectId } from "mongoose";
-import InvalidObjectIdError from "../errors/InvalidObjectIdError";
+import { isValidObjectId } from 'mongoose';
+import PostModel from '../models/PostModel';
+import NotFoundError from '../errors/NotFoundError';
+import BaseCrudController from './BaseCrudController';
+import InvalidObjectIdError from '../errors/InvalidObjectIdError';
 
-class PostController implements BaseCrudController<IPost> {
+class PostController implements BaseCrudController {
   /** @override */
-  public async create(req: Request, res: Response, next: NextFunction) {
+  public static async create(req: Request, res: Response, next: NextFunction) {
     const {
-        body,
-        title,
-      } = req.body;
+      body,
+      title,
+    } = req.body;
 
     const post = new PostModel({
       body,
-      title
+      title,
     });
 
     try {
-      const { title, body, _id } = await post.save();
-      res.status(StatusCodes.CREATED).json({ title, body, _id });
-    } catch(error: any) {
+      const savedPost = await post.save();
+      res.status(StatusCodes.CREATED).json({
+        title: savedPost.title,
+        _id: savedPost._id,
+        body: savedPost.body,
+      });
+    } catch (error: any) {
       next(error);
     }
   }
+
   /** @override */
-  public async getOne(req: Request, res: Response, next: NextFunction) {
+  public static async getOne(req: Request, res: Response, next: NextFunction) {
     const { postId } = req.params;
 
     try {
-      if(!isValidObjectId(postId)) throw new InvalidObjectIdError();
+      if (!isValidObjectId(postId)) throw new InvalidObjectIdError();
 
       const post = await PostModel.findById(postId);
-      if(!post) throw new NotFoundError("Post not found.");
+      if (!post) throw new NotFoundError('Post not found.');
 
       res.status(StatusCodes.OK).json({ post });
-    } catch(error) {
+    } catch (error) {
       next(error);
     }
   }
+
   /** @override */
-  public async getAll(req: Request, res: Response, next: NextFunction) {
+  public static async getAll(req: Request, res: Response, next: NextFunction) {
     try {
       const posts = await PostModel.find({});
 
-    res.status(StatusCodes.OK).json({ posts });
-    } catch(error) {
+      res.status(StatusCodes.OK).json({ posts });
+    } catch (error) {
       next(error);
     }
   }
+
   /** @override */
-  public async update(req: Request, res: Response, next: NextFunction) {
-    const { postId }= req.params;
+  public static async update(req: Request, res: Response, next: NextFunction) {
+    const { postId } = req.params;
     const { body: newBody, title: newTitle } = req.body;
 
     try {
-      if(!isValidObjectId(postId)) throw new InvalidObjectIdError();
+      if (!isValidObjectId(postId)) throw new InvalidObjectIdError();
       const post = await PostModel.findById(postId);
-      
-      if(!post) throw new NotFoundError("Post not found.");
+
+      if (!post) throw new NotFoundError('Post not found.');
 
       await post.set({
         body: newBody,
-        title: newTitle
+        title: newTitle,
       });
 
-      const { title, body, _id } = await post.save();
+      const updatedPost = await post.save();
 
-      res.status(StatusCodes.OK).json({title, _id, body});
-    } catch(error) {
+      res.status(StatusCodes.OK).json({
+        title: updatedPost.title,
+        _id: updatedPost._id,
+        body: updatedPost.body,
+      });
+    } catch (error) {
       next(error);
     }
   }
+
   /** @override */
-  public async delete(req: Request, res: Response, next: NextFunction) {
+  public static async delete(req: Request, res: Response, next: NextFunction) {
     const { postId } = req.params;
 
     try {
-      if(!isValidObjectId(postId)) throw new InvalidObjectIdError();
+      if (!isValidObjectId(postId)) throw new InvalidObjectIdError();
       const post = await PostModel.findById(postId);
 
-      if(!post) throw new NotFoundError("Post not found.");
+      if (!post) throw new NotFoundError('Post not found.');
 
       await post.delete();
       res.sendStatus(StatusCodes.OK);
