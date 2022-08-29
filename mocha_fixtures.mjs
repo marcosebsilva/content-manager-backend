f643f26f322b9de6c8722ee0e70366320fb041f2
marcos-silva <dusdoom.dev@gmail.com>
2022-08-29 13:12:45 -0300

    chore: created test config files

---
+++ mocha_fixtures.mjs
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+dotenv.config();
+
+const memoryServer = await MongoMemoryServer.create();
+const URI = memoryServer.getUri();
+
+/** Runs before tests: 
+* connects to the memory database created with {@link MongoMemoryServer}
+*/
+export async function mochaGlobalSetup() {
+  try {
+    await mongoose.connect(URI);
+    console.log('Mongoose test environment connected!');
+  } catch (error) {
+    console.log('Mongoose test environtment connection failed!');
+  }
+};
+
+/**Makes sure to disconnect from {@link memoryServer} */
+export async function mochaGlobalTeardown() {
+  try {
+    await mongoose.disconnect();
+    console.log("Mongoose test environment connection finished!");
+  } catch (error) {
+    console.log("Mocha teardown failed.");
+  }
+}
