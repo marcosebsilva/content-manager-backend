da02f33715e58041f77cf73817ef0b70fee44016
marcos-silva <dusdoom.dev@gmail.com>
2022-08-29 17:00:19 -0300

    fix(middleware): fixed missing fourth parameter from error middleware

--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -1,18 +1,19 @@
-import { NextFunction, Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { Error } from 'mongoose';
-import clearValidationErrors from "../utils/clearValidationErrors";
+import clearValidationErrors from '../utils/clearValidationErrors';
 
-export default function (error: any, req: Request, res: Response, next: NextFunction) {
-  if (error instanceof Error.ValidationError){
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function (error: any, req: Request, res: Response, _next: NextFunction) {
+  if (error instanceof Error.ValidationError) {
     const errors = clearValidationErrors(error);
     res.status(StatusCodes.BAD_REQUEST).json({ errors });
     return;
   }
 
   try {
-    res.status(error.status).json({message: error.message});
+    res.status(error.status).json({ message: error.message });
   } catch {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: "A internal error occurred."});
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'A internal error occurred.' });
   }
 }
