a959077a06fcb4996179cc13406240d058750bba
marcos-silva <dusdoom.dev@gmail.com>
2022-08-30 18:02:44 -0300

    chore: removed unused files

--- src/errors/BadRequestError.ts
+++
@@ -1,9 +0,0 @@
-import { StatusCodes } from 'http-status-codes';
-import BaseError from './BaseError';
-
-/** Throwable error used when a request param is not valid */
-export default class BadRequestError extends BaseError {
-  constructor(message: string) {
-    super(message, 'BadRequestError', StatusCodes.BAD_REQUEST);
-  }
-}
